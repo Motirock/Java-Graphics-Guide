b5dbc3a136819988cfde0ef8ade019a97e537a55
Motirock <117296582+Motirock@users.noreply.github.com>
2023-03-13 14:47:38 -0400

    Update ImageUtils.java

--- ImageUtils.java
+++ ImageUtils.java
@@ -1,10 +1,15 @@
+/*
+Use this class to store methods used for images.
+I currently have only one method used for checking rotating an image about a point.
+*/
+
 package main;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class ImageUtils {
-    //Rotates an image around a point
+    //Rotates an image about a point
     public static void drawRotatedImage(Graphics2D g2, BufferedImage image, int degrees, int x, int y, int width, int height, int pivotX, int pivotY, double GS) {
         g2.rotate(Math.toRadians(degrees), (int) (pivotX*GS), (int) (pivotY*GS));
         g2.drawImage(image, (int) (x*GS), (int) (y*GS), (int) (width*GS), (int) (height*GS), null);
