b08924f9bc8b36d8b4ed499749695020dab22125
Motirock <117296582+Motirock@users.noreply.github.com>
2023-03-13 14:47:22 -0400

    Update CollisionChecker.java

--- CollisionChecker.java
+++ CollisionChecker.java
@@ -1,6 +1,6 @@
 /*
 Use this class to store methods used for checking collisions.
-I currently have only one for checking collisions of Rectangles.
+I currently have only one method used for checking collisions of Rectangles.
 */
 
 package main;
