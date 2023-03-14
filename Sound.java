67cb6fd6f761e5936f5c1666e808143b52719c8d
Motirock <117296582+Motirock@users.noreply.github.com>
2023-03-14 09:31:00 -0400

    Update Sound.java

--- Sound.java
+++ Sound.java
@@ -8,16 +8,18 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 
 public class Sound {
-    URL soundURL[] = new URL[30];
-    Clip clip;
-    public int volume = 50;
-    FloatControl fc;
-
+    URL soundURL[] = new URL[30];   //By default, it can store 30 sounds
+    Clip clip;                      //Used to play audio
+    public int volume = 50;         //Volume: default is 50%. I made sure it is linear instead of in decibels (logarithmic)
+    FloatControl fc;                //Used to help control volume
+    
+    //Loads URLs of sounds
     public Sound() {
-        //soundURL[0] = getClass().getResource("/res/audio/bruh.wav");
-        //soundURL[1] = getClass().getResource("/res/audio/enemy_death.wav");
+        //soundURL[0] = getClass().getResource("/res/audio/example_1.wav");
+        //soundURL[1] = getClass().getResource("/res/audio/example_2.wav");
     }
-
+    
+    //Sets the file being played
     public void setFile(int i) {
         try {
             AudioInputStream sound = AudioSystem.getAudioInputStream(soundURL[i]);
@@ -27,24 +29,29 @@ public class Sound {
             updateVolume();
         } catch(Exception e) {}
     }
-
+    
+    //Plays the current sound from the beginning
     public void play() {
         clip.setFramePosition(0);
         clip.start();
     }
-
+    
+    //Loops the current sound
     public void loop() {
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
-
+    
+    //Stops the current sounds
     public void stop() {
         clip.stop();
     }
-
+    
+    //Updates volume of the FloatControl to the current linear-scaled volume
     void updateVolume() {
         fc.setValue((float) (Math.log10(Math.pow(10, volume))/100*86-80));
     }
-
+    
+    //Sets volume to an integer 0-100 on a logarthmic scale
     public void setVolume(int vol) {
         volume = vol;
         updateVolume();
