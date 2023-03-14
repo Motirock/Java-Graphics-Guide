158867fa3ee1488837c39dfaa5bfc8a833ef1e8d
Motirock <117296582+Motirock@users.noreply.github.com>
2023-03-14 09:04:30 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 ///////////////////////////////////////
 //  |         |     -----     |---\  //
-//  |    |    |       |       |   |  //
-//  |    |    |       |       |---/  //
+//  |    |    |       |       |   |  //         //Place your important
+//  |    |    |       |       |---/  //         //information up here
 //   \__/ \__/      -----     |      //
 ///////////////////////////////////////
+
+/*
+The main class doesn't actually do much, it just creates the window.
+Main creates the GamePanel, which coordinates the internal workings of the application.
+*/
 
 package main;
 
@@ -17,21 +22,21 @@ public class Main{
         System.out.println("Hello, World!");
         
         //Setting properties of window
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
-        window.setTitle("Sand Simulator");
-        window.setFocusTraversalKeysEnabled(false); 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //Closes application when X is pressed
+        window.setResizable(false);                             //Can't be manually resized
+        window.setTitle("My Java Application");                 //Window title
+        window.setFocusTraversalKeysEnabled(false);             //Disables certain keys, like tab, from having special use
 
         //Adding gamePanel, which actually controls the application
-        GamePanel gamePanel = new GamePanel();
+        GamePanel gamePanel = new GamePanel();                  
         window.add(gamePanel);
 
         window.pack();
 
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
+        window.setLocationRelativeTo(null);     //Sets the windows position to the center of the screen by default
+        window.setVisible(true);                //Makes the window visible
 
-        gamePanel.startGameThread();
+        gamePanel.startGameThread();            //Starts execution of the game
     }
 }
 
