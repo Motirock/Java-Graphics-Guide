0262764f35cec0e11575a04b2c22cdee8755e195
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2023-05-15 13:03:17 -0400

    Update ImageUtils.java

--- Template/main/ImageUtils.java
+++ Template/main/ImageUtils.java
@@ -11,8 +11,11 @@ import java.awt.image.BufferedImage;
 public class ImageUtils {
     //Rotates an image about a point
     public static void drawRotatedImage(Graphics2D g2, BufferedImage image, int degrees, int x, int y, int width, int height, int pivotX, int pivotY, double GS) {
+        //Rotates
         g2.rotate(Math.toRadians(degrees), (int) (pivotX*GS), (int) (pivotY*GS));
+        //Draws
         g2.drawImage(image, (int) (x*GS), (int) (y*GS), (int) (width*GS), (int) (height*GS), null);
+        //Rotates back to the normal orientation
         g2.rotate(Math.toRadians(-degrees), (int) (pivotX*GS), (int) (pivotY*GS));
     }
 }
