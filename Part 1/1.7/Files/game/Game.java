e6f81e361e90644e8e17cc88aa8abad3f0514acf
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2023-05-16 13:16:25 -0400

    Add files via upload

---
+++ Part 1/1.7/Files/game/Game.java
@@ -0,0 +1,52 @@
+package game;
+
+import main.GamePanel;
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class Game {
+    private GamePanel gp;
+
+    private long updates = 0; //Total times update() has been run
+
+    //A BufferedImage which does not store anything yet
+    public BufferedImage chestImage = null;
+
+    //Position and dimensions of the image
+    private int x = 350;    //Top left corner x
+    private int y = 100;    //Top left corner y
+    private int w = 800;    //Image width
+    private int h = 800;    //Image height
+
+    public Game(GamePanel gp) {
+        this.gp = gp;
+        
+        //Loads the chest image from the res folder to image
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/res/chest.png"));
+        } catch (IOException e) {
+            //Prints the error and the stack trace if it fails to load the image from res
+            e.printStackTrace();
+        }
+    }
+    
+    public update() {
+        //Keeps track of total times update() was called
+        updates++;
+
+        //The image moves right and then left, switching direction every second
+        if (updates % 10 == 0) {
+            if (updates % 2000 < 1000)
+                x++;    //Moving right
+            else
+                x--;    //Moving left
+        }
+    }
+    
+    public draw(Graphics2D g2) {
+        //Draws our text at the coordinates given.
+        g2.drawString(str, xCoordinate, yCoordinate);
+    }
+}
