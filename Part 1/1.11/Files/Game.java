a6cbf867649011459b6331a4a12b01ccfbeec45e
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2023-05-24 13:42:34 -0400

    Add files via upload

---
+++ Part 1/1.11/Files/Game.java
@@ -0,0 +1,23 @@
+package game;
+
+import main.GamePanel;
+
+public class Game {
+    private GamePanel gp;
+
+    private long updates = 0;
+    
+    public Game(GamePanel gp) {
+        this.gp = gp;
+    }
+    
+    public update() {
+        updates++;
+
+        
+    }
+    
+    public draw(Graphics2D g2, double GS) {
+        
+    }
+}
