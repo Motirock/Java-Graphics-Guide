13c65fb1cb8ff7ca2bf1174f8e5c489895e3ce4a
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2023-05-24 13:37:24 -0400

    Update Game.java

--- Part 1/1.10/Files/Game.java
+++ Part 1/1.10/Files/Game.java
@@ -1,1 +1,50 @@
-# 1.10
+package game;
+
+import main.GamePanel;
+
+public class Game {
+    private GamePanel gp;
+
+    private long updates = 0;
+
+    //Keeps track of rectangle position
+    private double centerX = 800, centerY = 450;
+    private int width = 100, height = 100;
+    
+    public Game(GamePanel gp) {
+        this.gp = gp;
+    }
+    
+    public update() {
+        updates++;
+
+        //Keyboard controls, where each arrow key moves the rectangle accordingly
+        if (gp.keyH.upArrowPressed) {
+            centerY -= 0.1;
+        }
+        if (gp.keyH.downArrowPressed) {
+            centerY += 0.1;
+        }
+        if (gp.keyH.leftArrowPressed) {
+            centerX -= 0.1;
+        }
+        if (gp.keyH.rightArrowPressed) {
+            centerX += 0.1;
+        }
+
+        //Keeps rectangle from going out of bounds
+        if (centerX < width)
+            centerX = width;
+        if (centerX > 1600-width)
+            centerX = 1600-width;
+        if (centerY < height)
+            centerY = height;
+        if (centerY > 900-height)
+            centerY = height;
+    }
+    
+    public draw(Graphics2D g2, double GS) {
+        //Draws the rectangle
+        g2.fillRectangle((int) ((centerX-width/2.0)*GS), (int) ((centerY-height/2.0)*GS), width, height);
+    }
+}
