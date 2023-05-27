de63b1d8f4746ba345d37f6d7dac73fff5c7e1a7
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2023-05-27 08:37:35 -0400

    Update Game.java

--- Tutorials/Part 5/Files/Game.java
+++ Tutorials/Part 5/Files/Game.java
@@ -3,22 +3,22 @@ package game;
 import main.GamePanel;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 
 public class Game {
     private GamePanel gp;
 
     private long updates = 0; //Total times update() has been run
-    private int radius = 100;
 
     public Game(GamePanel gp) {
         this.gp = gp;
     }
     
-    public update() {
+    public void update() {
         updates++; //Note that by default 1000 updates per second are attempted
     }
     
-    public draw(Graphics2D g2, double GS) {
+    public void draw(Graphics2D g2, double GS) {
         g2.setColor(Color.BLUE);
         g2.fillOval(400, 50, 800, 800);
         g2.setColor(Color.GREEN);
