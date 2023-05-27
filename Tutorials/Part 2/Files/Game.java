8ff9172b03e986880d478f7c078de09478467950
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2023-05-27 08:33:10 -0400

    Update Game.java

--- Tutorials/Part 2/Files/Game.java
+++ Tutorials/Part 2/Files/Game.java
@@ -2,18 +2,20 @@ package game;
 
 import main.GamePanel;
 
+import java.awt.Graphics2D;
+
 public class Game {
+    private long updates = 0;
+
     private GamePanel gp;
 
     public Game(GamePanel gp) {
         this.gp = gp;
     }
-    
-    public update() {
-    
+
+    public void update() {
+        updates++;
     }
-    
-    public draw(Graphics2D g2, double GS) {
-    
-    }
+
+    public void draw(Graphics2D g2, double GS) {}
 }
