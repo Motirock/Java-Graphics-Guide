b6627122f534403be4f1c272b03b83eab7d4ae5e
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2023-12-30 14:14:56 -0500

    Document Rectangle.java

--- Template/main/Rectangle.java
+++ Template/main/Rectangle.java
@@ -1,3 +1,15 @@
+/*
+Rectangle objects are super useful, so here's a simple class for doing so.
+This class is also used by the CollisionChecker class.
+Please note that just like graphics, its intended use is based around the top-left corner.
+This means a Rectangle created like
+    new Rectangle(100, 100, 200, 300)
+has its left edge at 100, its top edge at 100, a width of 200, and a height of 300
+This would also mean that said rectangle's bottom left corner would be at (300, 400)
+
+In theory, you could have x and y represent the center coordinates, but then using Graphics2D would be a bit more complicated
+*/
+
 package main;
 
 public class Rectangle {
