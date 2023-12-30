5573b1d8326ee24f1a2a05a9f40e1b2397a12fc5
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2023-12-30 14:03:55 -0500

    Rectangle Class
    Simpler than javafx class, also not always installed with the rest of the normal java libraries

---
+++ Template/main/Rectangle.java
@@ -0,0 +1,48 @@
+package main;
+
+public class Rectangle {
+    public double x, y, width, height;
+
+    public Rectangle(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public boolean intersects(Rectangle other) {
+        return x < other.x + other.width && x + width > other.x && y < other.y + other.height && y + height > other.y;
+    }
+}
